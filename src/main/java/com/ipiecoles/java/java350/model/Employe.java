b6101093c211132242a92fdbbea0a6ab700851bb
IDyapiI <maelalle@outlook.fr>
2019-03-22 16:54:30 +0100

    fix bug on getNbRtt
    Signed-off-by: IDyapiI <maelalle@outlook.fr>

--- src/main/java/com/ipiecoles/java/java350/model/Employe.java
+++ src/main/java/com/ipiecoles/java/java350/model/Employe.java
@@ -11,7 +11,6 @@ import java.util.Objects;
 @Entity
 public class Employe {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -33,7 +32,8 @@ public class Employe {
     public Employe() {
     }
 
-    public Employe(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Integer performance, Double tempsPartiel) {
+    public Employe(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire,
+            Integer performance, Double tempsPartiel) {
         this.nom = nom;
         this.prenom = prenom;
         this.matricule = matricule;
@@ -44,14 +44,17 @@ public class Employe {
     }
 
     /**
-     * Retourne le nombre d'année d'ancienneté de l'employé par rapport à sa date d'embauche (on ne prend pas en compte
-     * les mois et les jours. Il faut en revanche que la d'embauche soit non nulle et l'année antérieure à l'année courante
-     * sinon on renvoie une ancienneté de 0
+     * Retourne le nombre d'année d'ancienneté de l'employé par rapport à sa date
+     * d'embauche (on ne prend pas en compte les mois et les jours. Il faut en
+     * revanche que la d'embauche soit non nulle et l'année antérieure à l'année
+     * courante sinon on renvoie une ancienneté de 0
      *
      * @return le nombre d'année d'ancienneté
      */
     public final Integer getNombreAnneeAnciennete() {
-        return dateEmbauche != null && LocalDate.now().getYear() >= dateEmbauche.getYear() ? LocalDate.now().getYear() - dateEmbauche.getYear() : 0;
+        return dateEmbauche != null && LocalDate.now().getYear() >= dateEmbauche.getYear()
+                ? LocalDate.now().getYear() - dateEmbauche.getYear()
+                : 0;
     }
 
     public Integer getNbConges() {
@@ -59,65 +62,81 @@ public class Employe {
     }
 
     /**
-     * Nombre de jours de RTT =
-     *   Nombre de jours dans l'année
-     * – plafond maximal du forfait jours de la convention collective
-     * – nombre de jours de repos hebdomadaires
-     * – jours de congés payés
-     * – nombre de jours fériés tombant un jour ouvré
+     * Nombre de jours de RTT = Nombre de jours dans l'année – plafond maximal du
+     * forfait jours de la convention collective – nombre de jours de repos
+     * hebdomadaires – jours de congés payés – nombre de jours fériés tombant un
+     * jour ouvré
      *
      * Au prorata de son pourcentage d'activité (arrondi au supérieur)
      *
      * @return le nombre de jours de RTT
      */
-    public Integer getNbRtt(){
+    public Integer getNbRtt() {
         return getNbRtt(LocalDate.now());
     }
 
-    public Integer getNbRtt(LocalDate d){
+    public Integer getNbRtt(LocalDate d) {
         int i1 = d.isLeapYear() ? 365 : 366;
         int var = 104;
-        switch (LocalDate.of(d.getYear(),1,1).getDayOfWeek()){
-            case THURSDAY: if(d.isLeapYear()) var =  var + 1; break;
-            case FRIDAY: if(d.isLeapYear()) var =  var + 2; else var =  var + 1;
-            case SATURDAY: var = var + 1; break;
+        switch (LocalDate.of(d.getYear(), 1, 1).getDayOfWeek()) {
+        case THURSDAY:
+            if (d.isLeapYear())
+                var = var + 1;
+            break;
+        case FRIDAY:
+            if (d.isLeapYear())
+                var = var + 2;
+            else
+                var = var + 1;
+            break;
+        case SATURDAY:
+            var = var + 1;
+            break;
         }
-        int monInt = (int) Entreprise.joursFeries(d).stream().filter(localDate -> localDate.getDayOfWeek().getValue() <= DayOfWeek.FRIDAY.getValue()).count();
-        return (int) Math.ceil((i1 - Entreprise.NB_JOURS_MAX_FORFAIT - var - Entreprise.NB_CONGES_BASE - monInt) * tempsPartiel);
+        int monInt = (int) Entreprise.joursFeries(d).stream()
+                .filter(localDate -> localDate.getDayOfWeek().getValue() <= DayOfWeek.FRIDAY.getValue()).count();
+        return (int) Math
+                .ceil((i1 - Entreprise.NB_JOURS_MAX_FORFAIT - var - Entreprise.NB_CONGES_BASE - monInt) * tempsPartiel);
     }
 
     /**
-     * Calcul de la prime annuelle selon la règle :
-     * Pour les managers : Prime annuelle de base bonnifiée par l'indice prime manager
-     * Pour les autres employés, la prime de base plus éventuellement la prime de performance calculée si l'employé
-     * n'a pas la performance de base, en multipliant la prime de base par un l'indice de performance
-     * (égal à la performance à laquelle on ajoute l'indice de prime de base)
+     * Calcul de la prime annuelle selon la règle : Pour les managers : Prime
+     * annuelle de base bonnifiée par l'indice prime manager Pour les autres
+     * employés, la prime de base plus éventuellement la prime de performance
+     * calculée si l'employé n'a pas la performance de base, en multipliant la prime
+     * de base par un l'indice de performance (égal à la performance à laquelle on
+     * ajoute l'indice de prime de base)
      *
-     * Pour tous les employés, une prime supplémentaire d'ancienneté est ajoutée en multipliant le nombre d'année
-     * d'ancienneté avec la prime d'ancienneté. La prime est calculée au pro rata du temps de travail de l'employé
+     * Pour tous les employés, une prime supplémentaire d'ancienneté est ajoutée en
+     * multipliant le nombre d'année d'ancienneté avec la prime d'ancienneté. La
+     * prime est calculée au pro rata du temps de travail de l'employé
      *
      * @return la prime annuelle de l'employé en Euros et cents
      */
-    public Double getPrimeAnnuelle(){
-        //Calcule de la prime d'ancienneté
+    public Double getPrimeAnnuelle() {
+        // Calcule de la prime d'ancienneté
         Double primeAnciennete = Entreprise.PRIME_ANCIENNETE * this.getNombreAnneeAnciennete();
         Double prime;
-        //Prime du manager (matricule commençant par M) : Prime annuelle de base multipliée par l'indice prime manager
-        //plus la prime d'anciennté.
-        if(matricule != null && matricule.startsWith("M")) {
+        // Prime du manager (matricule commençant par M) : Prime annuelle de base
+        // multipliée par l'indice prime manager
+        // plus la prime d'anciennté.
+        if (matricule != null && matricule.startsWith("M")) {
             prime = Entreprise.primeAnnuelleBase() * Entreprise.INDICE_PRIME_MANAGER + primeAnciennete;
         }
-        //Pour les autres employés en performance de base, uniquement la prime annuelle plus la prime d'ancienneté.
-        else if (this.performance == null || Entreprise.PERFORMANCE_BASE.equals(this.performance)){
+        // Pour les autres employés en performance de base, uniquement la prime annuelle
+        // plus la prime d'ancienneté.
+        else if (this.performance == null || Entreprise.PERFORMANCE_BASE.equals(this.performance)) {
             prime = Entreprise.primeAnnuelleBase() + primeAnciennete;
         }
-        //Pour les employés plus performance, on bonnifie la prime de base en multipliant par la performance de l'employé
+        // Pour les employés plus performance, on bonnifie la prime de base en
+        // multipliant par la performance de l'employé
         // et l'indice de prime de base.
         else {
-            prime = Entreprise.primeAnnuelleBase() * (this.performance + Entreprise.INDICE_PRIME_BASE) + primeAnciennete;
+            prime = Entreprise.primeAnnuelleBase() * (this.performance + Entreprise.INDICE_PRIME_BASE)
+                    + primeAnciennete;
         }
-        //Au pro rata du temps partiel.
-        return Math.round(prime * this.tempsPartiel * 100)/100.0;
+        // Au pro rata du temps partiel.
+        return Math.round(prime * this.tempsPartiel * 100) / 100.0;
     }
 
     public void augmenterSalaire(double pourcentage) {
@@ -221,16 +240,15 @@ public class Employe {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Employe)) return false;
+        if (this == o)
+            return true;
+        if (!(o instanceof Employe))
+            return false;
         Employe employe = (Employe) o;
-        return Objects.equals(id, employe.id) &&
-                Objects.equals(nom, employe.nom) &&
-                Objects.equals(prenom, employe.prenom) &&
-                Objects.equals(matricule, employe.matricule) &&
-                Objects.equals(dateEmbauche, employe.dateEmbauche) &&
-                Objects.equals(salaire, employe.salaire) &&
-                Objects.equals(performance, employe.performance);
+        return Objects.equals(id, employe.id) && Objects.equals(nom, employe.nom)
+                && Objects.equals(prenom, employe.prenom) && Objects.equals(matricule, employe.matricule)
+                && Objects.equals(dateEmbauche, employe.dateEmbauche) && Objects.equals(salaire, employe.salaire)
+                && Objects.equals(performance, employe.performance);
     }
 
     @Override
