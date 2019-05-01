e90f15fce6682b51d0e34869b795d2da475352a7
IDyapiI <maelalle@outlook.fr>
2019-05-01 15:36:18 +0200

    feat integration test for calculPerformance
    Signed-off-by: IDyapiI <maelalle@outlook.fr>

--- src/test/java/com/ipiecoles/java/java350/service/EmployeServiceIntegrationTest.java
+++ src/test/java/com/ipiecoles/java/java350/service/EmployeServiceIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.ipiecoles.java.java350.service;
 
-
 import com.ipiecoles.java.java350.exception.EmployeException;
 import com.ipiecoles.java.java350.model.*;
 import com.ipiecoles.java.java350.repository.EmployeRepository;
@@ -15,7 +14,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -29,13 +27,13 @@ public class EmployeServiceIntegrationTest {
 
     @BeforeEach
     @AfterEach
-    public void setup(){
+    public void setup() {
         employeRepository.deleteAll();
     }
 
     @Test
     public void integrationEmbaucheEmploye() throws EmployeException {
-        //Given
+        // Given
         employeRepository.save(new Employe("Doe", "John", "T12345", LocalDate.now(), Entreprise.SALAIRE_BASE, 1, 1.0));
         String nom = "Doe";
         String prenom = "John";
@@ -43,20 +41,40 @@ public class EmployeServiceIntegrationTest {
         NiveauEtude niveauEtude = NiveauEtude.BTS_IUT;
         Double tempsPartiel = 1.0;
 
-        //When
+        // When
         employeService.embaucheEmploye(nom, prenom, poste, niveauEtude, tempsPartiel);
 
-        //Then
+        // Then
         Employe employe = employeRepository.findByMatricule("T12346");
         Assertions.assertNotNull(employe);
         Assertions.assertEquals(nom, employe.getNom());
         Assertions.assertEquals(prenom, employe.getPrenom());
-        Assertions.assertEquals(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")), employe.getDateEmbauche().format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+        Assertions.assertEquals(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")),
+                employe.getDateEmbauche().format(DateTimeFormatter.ofPattern("yyyyMMdd")));
         Assertions.assertEquals("T12346", employe.getMatricule());
         Assertions.assertEquals(1.0, employe.getTempsPartiel().doubleValue());
 
-        //1521.22 * 1.2 * 1.0
+        // 1521.22 * 1.2 * 1.0
         Assertions.assertEquals(1825.46, employe.getSalaire().doubleValue());
+    }
+
+    @Test
+    public void integrationPerformanceCommercial() throws EmployeException {
+        // Given
+        String nom = "Jean";
+        String prenom = "Denis";
+        Double salaire = 2000.0;
+        String matricule = "C123456";
+        Long caTraite = 2200L;
+        Long objectifCa = 2000L;
+        Integer performance = 3;
+        Double tempsPartiel = 1.0;
+        employeRepository.save(new Employe(nom, prenom, matricule, LocalDate.now(), salaire, performance, tempsPartiel));
+        // When
+        employeService.calculPerformanceCommercial(matricule, caTraite, objectifCa);
+        // Then
+        Employe employe = employeRepository.findByMatricule(matricule);
+        Assertions.assertEquals(5, (int) employe.getPerformance());
     }
 
 }
